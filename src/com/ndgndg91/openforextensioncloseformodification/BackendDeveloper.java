c6282f18b30cd5318b0963be6a51bfe740a8b64a
donggil91 <donggil@sixshop.com>
2020-12-09 22:48:27 +0900

    <feat> ocp

---
+++ src/com/ndgndg91/openforextensioncloseformodification/BackendDeveloper.java
@@ -0,0 +1,8 @@
+package com.ndgndg91.openforextensioncloseformodification;
+
+public class BackendDeveloper {
+
+    public void developBackEnd() {
+        System.out.println("Let's develop back-end!");
+    }
+}
