e536a94a1e183195acb8bc6f9fef177269cbe25b
donggil91 <donggil@sixshop.com>
2020-12-09 23:32:16 +0900

    <feat> remove useless import

--- src/com/ndgndg91/Main.java
+++ src/com/ndgndg91/Main.java
@@ -2,11 +2,6 @@ package com.ndgndg91;
 
 import com.ndgndg91.liskovsubstitution.LspDriver;
 import com.ndgndg91.openforextensioncloseformodification.OcpDriver;
-import com.ndgndg91.singleresponsibility.CustomerServiceRepresentative;
-import com.ndgndg91.singleresponsibility.Designer;
-import com.ndgndg91.singleresponsibility.Developer;
-import com.ndgndg91.singleresponsibility.God;
-import com.ndgndg91.singleresponsibility.Planner;
 import com.ndgndg91.singleresponsibility.SrpDriver;
 
 public class Main {
